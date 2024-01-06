c9f10b7df5c8a6d2994383719b52dcb78c12b6a4
Haridhanya <61719163+Haridhanya@users.noreply.github.com>
2024-01-06 11:24:37 +0530

    This is eclipse code to test

---
+++ dummytest.java
@@ -0,0 +1,18 @@
+package test;
+
+public class dummytest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Hari");
+		int a=2;
+		int b=4;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+"\n"+b);
+
+	}
+
+}
